1ac24498f1d1eb70d885626fdf91f353f08c4382
Michelle Li <michellecarolynclark@gmail.com>
2017-03-17 08:41:58 -0700

    algorithm comparison chart

--- parkinsons_sklearn.py
+++ parkinsons_sklearn.py
@@ -16,20 +16,15 @@ from sklearn.svm import SVC
 # load the dataset (local path)
 url = "datasets/parkinsons_data.csv"
 # feature names
-features = ["name","MDVP:Fo(Hz)","MDVP:Fhi(Hz)","MDVP:Flo(Hz)","MDVP:Jitter(%)","MDVP:Jitter(Abs)","MDVP:RAP","MDVP:PPQ","Jitter:DDP","MDVP:Shimmer","MDVP:Shimmer(dB)","Shimmer:APQ3","Shimmer:APQ5","MDVP:APQ","Shimmer:DDA","NHR","HNR","status","RPDE","DFA","spread1","spread2","D2","PPE"]
+features = ["MDVP:Fo(Hz)","MDVP:Fhi(Hz)","MDVP:Flo(Hz)","MDVP:Jitter(%)","MDVP:Jitter(Abs)","MDVP:RAP","MDVP:PPQ","Jitter:DDP","MDVP:Shimmer","MDVP:Shimmer(dB)","Shimmer:APQ3","Shimmer:APQ5","MDVP:APQ","Shimmer:DDA","NHR","HNR","RPDE","DFA","spread1","spread2","D2","PPE","status"]
 dataset = pandas.read_csv(url, names = features)
-
-# dataset dimensions
-print(dataset.shape)
-# class distrubution (number of instances that belong to each class of flower)
-print(dataset.groupby('class').size())
 
 # store the dataset as an array for easier processing
 array = dataset.values
 # X stores feature values
-X = array[:,1:23]
-# Y stores "answers"
-Y = array[:,23]
+X = array[:,0:22]
+# Y stores "answers", the flower species / class (every row, 4th column)
+Y = array[:,22]
 validation_size = 0.20
 # randomize which part of the data is training and which part is validation
 seed = 7
@@ -43,12 +38,12 @@ seed = 7
 # use the 'accuracy' metric to evaluate models (correct / total)
 scoring = 'accuracy'
 
-# algorithms
+# algorithms / models
 models = []
 models.append(('LR', LogisticRegression()))
 models.append(('LDA', LinearDiscriminantAnalysis()))
 models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
+models.append(('DT', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC()))
 
@@ -69,13 +64,13 @@ fig.suptitle('Algorithm comparison')
 ax = fig.add_subplot(111)
 plt.boxplot(results)
 ax.set_xticklabels(names)
-plt.savefig('parkinsons_algorithm_comparison.png')
+plt.savefig('Algorithm-comparison.png')
 plt.show()
 
 # using KNN to make predictions about the validation set
-knn = KNeighborsClassifier()
-knn.fit(X_train, Y_train)
-predictions = knn.predict(X_validation)
+dt = DecisionTreeClassifier()
+dt.fit(X_train, Y_train)
+predictions = dt.predict(X_validation)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
